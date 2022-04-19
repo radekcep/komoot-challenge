26c8906f8ab5d91e283a7d2823a813fa62e19b3a
Radek Čep <31346847+radekcep@users.noreply.github.com>
2022-04-19 09:48:08 +0200

    Added Xcode project

---
+++ Sources/KomootChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomootChallenge.swift
+//  KomootChallenge
+//
+//  Created by Radek Čep on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct KomootChallenge: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
